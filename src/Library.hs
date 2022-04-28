214ba164b30cec87cdd1bf20437a88471d8faa10
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2022-04-28 09:53:52 -0300

    Arreglo esBisiesto

--- src/Library.hs
+++ src/Library.hs
@@ -2,7 +2,7 @@ module Library where
 import PdePreludat
 
 esBisiesto :: Number -> Bool
-esBisiesto anio = esMultiploDe 400 anio || esMultiploDe 4 anio && esMultiploDe 100 anio
+esBisiesto anio = esMultiploDe 400 anio || esMultiploDe 4 anio && not (esMultiploDe 100 anio)
 
 esMultiploDe :: Number -> Number -> Bool
 esMultiploDe divisor dividendo = mod dividendo divisor == 0
