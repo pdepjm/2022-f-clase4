499bf150cfa9de275ae3d196fc49f01cc131a0a1
lspigariol <lspigariol@gmail.com>
2022-04-28 12:07:07 -0300

    Co-authored-by: asanzo <asanzo@users.noreply.github.com>

--- src/Library.hs
+++ src/Library.hs
@@ -1,8 +1,111 @@
 module Library where
 import PdePreludat
 
+
+doble nro = nro * 2
+
 esBisiesto :: Number -> Bool
 esBisiesto anio = esMultiploDe 400 anio || esMultiploDe 4 anio && not (esMultiploDe 100 anio)
 
 esMultiploDe :: Number -> Number -> Bool
 esMultiploDe divisor dividendo = mod dividendo divisor == 0
+
+data Tomate = UnTomate {
+    caracteristicas:: [String],
+    peso:: Number 
+} deriving (Show,Eq)
+
+tomate1 = UnTomate ["grande", "lindo","rojo"] 50
+tomate2 = UnTomate ["rojo","maduro"] 60
+tomate3 = UnTomate [] 121
+
+algunosTomates = [tomate1,tomate2,tomate3]
+-- Primer ejemplo de necesidad de crear mi función de orden superior:
+
+-- Accesorio de corte
+cortar :: Tomate -> Number
+cortar tomate = div (peso tomate) 10 
+
+-- Máquina cortadora (v1)
+-- cortadora tomates = sum (map cortar tomates) - length tomates
+-- Máquina cortadora (v2)
+cortadora :: [ Tomate ] -> Number
+cortadora tomates = multiprocesadora cortar tomates 
+
+
+-- Accesorio de rayadura
+rayar :: Tomate -> Number
+rayar tomate  = min (peso tomate) 100
+
+-- Máquina rayadora (v1)
+-- rayadora tomates = sum (map rayar tomates) - length tomates
+-- Máquina rayadora (v2)
+rayadora :: [ Tomate ] -> Number
+rayadora tomates = multiprocesadora rayar tomates
+
+
+-- Máquina genérica que recibe el accesorio ¡en lugar de las máquinas anteriores!
+-- Construyo mi propia función de orden superior
+multiprocesadora:: (Tomate -> Number)  -> [Tomate] -> Number
+multiprocesadora accesorio tomates = sum (map accesorio tomates ) - length tomates
+
+-- OTRO EJEMPLO de necesidad de construir mi función de orden superior:
+maduro :: Tomate -> Bool
+maduro tomate = elem "maduro" (caracteristicas tomate)
+
+-- Laburo manual con cuchillo (v1)
+-- trabajaConCuchillo tomate 
+-- | maduro tomate = cortar tomate 
+-- | otherwise = 1
+-- Laburo manual con cuchillo (v2)
+trabajaConCuchillo:: Tomate -> Number
+trabajaConCuchillo tomate = trabajaCon cortar tomate
+
+-- Laburo manual con rayador
+trabajaConRayador:: Tomate -> Number
+trabajaConRayador tomate 
+ | maduro tomate = rayar tomate 
+ | otherwise = 1
+ 
+-- Laburo manual genérico (¡en lugar de las anteriores!)
+-- Construyo mi propia función de orden superior
+trabajaCon :: (Tomate -> Number) -> Tomate ->  Number
+trabajaCon accesorio tomate 
+  | maduro tomate = accesorio tomate
+  | otherwise = 1
+
+
+-- Los cocineros
+data Cocinero = UnCocinero {
+  nombre :: String,
+  accesorioPreferido:: Tomate -> Number
+}
+
+german = UnCocinero "German" cortar
+donato = UnCocinero "Donato" rayar
+
+cocinaMucho:: Cocinero -> [Tomate] -> Bool
+cocinaMucho cocinero tomates = (multiprocesadora (accesorioPreferido cocinero) tomates) > 50
+
+
+losDosPrimeros palabra = take 2 palabra
+-------------------------------
+olla :: String -> Tomate -> Tomate
+olla liquido tomate = 
+    UnTomate ("cocido":caracteristicas tomate) 
+             (peso tomate * incremento liquido)
+
+incremento "aceite" = 1.1
+incremento "agua" = 1.2
+
+algo f = f 3
+
+freir = olla "aceite"
+
+esPar = even
+
+mitad = (/2)
+
+anioActual = 2022
+--hervir tomate = olla "agua" tomate
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -21,4 +21,24 @@ correrTests = hspec $ do
       esBisiesto 300 `shouldBe` False
     it "Un año que no cumpla ninguna de las anteriores no debe ser bisiesto" $ do
       esBisiesto 1997 `shouldBe` False
-
+  describe "Tests de tomates" $ do
+    it "Al cortar un tomate obtengo la cantidad de partes, la décima parte de los gramos" $ do
+      cortar tomate1 `shouldBe` 5
+      cortar tomate3 `shouldBe` 12
+    it "La cortadora suma toda las partes y le resta la cantidad" $ do
+      cortadora [tomate1,tomate2,tomate3] `shouldBe` 5 + 6 + 12 - 3
+    it "La rayadora de tomates raya en pedacitos por gramo" $ do
+      rayar tomate1 `shouldBe` 50
+    it "Debería tener máximo 100" $ do
+      rayar tomate3 `shouldBe` 100
+    it "Laburo manual cortando debería cortar si es maduro" $ do
+      trabajaCon cortar tomate2 `shouldBe` 6
+    it "Laburo manual cortando debería dejarlo entero si no es maduro" $ do
+      trabajaCon rayar tomate1 `shouldBe` 1
+    it "Un cocinero cocina mucho si corta en muchas partes (más de 50)" $ do
+      cocinaMucho donato algunosTomates `shouldBe` True
+    it "Un cocinero no cocina mucho si no corta en muchas partes (menos de 50)" $ do
+      cocinaMucho german algunosTomates `shouldBe` False
+  describe "Tests de ollas" $ do
+    it "Una olla cuece y humedece el tomate" $ do
+      olla "aceite" tomate1 `shouldBe` UnTomate ["cocido","grande","lindo","rojo"] 55
