aed66405f28fa077f6e9c5b3a2ea818691b3ded2
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2022-04-28 09:49:52 -0300

    primeros tests

--- src/Library.hs
+++ src/Library.hs
@@ -2,7 +2,7 @@ module Library where
 import PdePreludat
 
 esBisiesto :: Number -> Bool
-esBisiesto anio = esMultiploDe anio 400 || esMultiploDe anio 4 && not (esMultiploDe anio 100)
+esBisiesto anio = esMultiploDe 400 anio || esMultiploDe 4 anio && esMultiploDe 100 anio
 
 esMultiploDe :: Number -> Number -> Bool
 esMultiploDe divisor dividendo = mod dividendo divisor == 0
--- src/Spec.hs
+++ src/Spec.hs
@@ -12,4 +12,13 @@ correrTests = hspec $ do
       esMultiploDe 46 2 `shouldBe` False
     it "esMultiplo da falso cuando no es múltiplo" $ do
       esMultiploDe 3 46 `shouldBe` False
+  describe "Tests de esBisiesto" $ do
+    it "Un año múltiplo de 400 es bisiesto" $ do
+      esBisiesto 2000 `shouldBe` True
+    it "Un año múltiplo de 4 y no de 100 es bisiesto" $ do
+      esBisiesto 2020 `shouldBe` True
+    it "Un año múltiplo de 100 pero no de 400 no es bisiesto" $ do
+      esBisiesto 300 `shouldBe` False
+    it "Un año que no cumpla ninguna de las anteriores no debe ser bisiesto" $ do
+      esBisiesto 1997 `shouldBe` False
 
